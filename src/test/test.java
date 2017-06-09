a0b28e520b99464f84a215a80e6887535f9c657e
Admin <aaa@github.com>
2017-06-09 17:46:47 +0800

    444

--- src/test/test.java
+++ src/test/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args){
         System.out.println("123");
         System.out.println("234");
+        System.out.println("4556");
     }
 }
